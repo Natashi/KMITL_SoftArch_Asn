9d402e5a450ddf37a674fc2bd0e16a0aae340fb2
Natashi <32347635+Natashi@users.noreply.github.com>
2022-10-12 16:22:57 +0700

    lab solid

---
+++ LabSOLID/lsp/Square.java
@@ -0,0 +1,19 @@
+public class Square extends Shape {
+    private int side;
+    
+    Square(int side) {
+        this.setSide(side);
+    }
+    
+    public void setSide(int side) {
+        this.side = side;
+    }
+    public int getSide() {
+        return side;
+    }
+    
+    @Override
+    public int getArea() {
+        return side * side;
+    }
+}
